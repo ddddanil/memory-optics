138718db7ff7e70e6e2a40e530da2745a4d6cb5e
Danil Doroshin <ddddanil5555@gmail.com>
2024-05-04 14:36:05 +0300

    feat: offset lens

--- src/Data/Memory.hs
+++ src/Data/Memory.hs
@@ -1,22 +1,33 @@
 module Data.Memory
-( Pointer(Offset)
+( Pointer(Offset, addOffset, subOffset)
 , Memory(ReadMem, WriteMem), readMem, writeMem
 , runMemoryToIO
 )
 where
 
-import           Data.Kind        (Type)
-import qualified Foreign.Ptr      as GHC
-import           Foreign.Storable (Storable (peek, poke))
-import           Polysemy         (Embed, InterpreterFor, Member, interpret,
-                                   makeSem)
-import           Polysemy.Embed   (embed)
+import           Control.Lens.Lens (Lens', lens)
+import           Data.Kind         (Type)
+import qualified Foreign.Ptr       as GHC
+import           Foreign.Storable  (Storable (peek, poke))
+import           Polysemy          (Embed, InterpreterFor, Member, interpret,
+                                    makeSem)
+import           Polysemy.Embed    (embed)
 
 class Storable a => Pointer p a where
   data Offset p b :: Type
+  addOffset :: Storable b => p a -> Offset p b -> p b
+  subOffset :: Storable b => p b -> Offset p b -> p a
 
 instance Storable a => Pointer GHC.Ptr a where
   data Offset GHC.Ptr b = GHCPtrOffset Int
+  addOffset p (GHCPtrOffset o) = p `GHC.plusPtr` o
+  subOffset p (GHCPtrOffset o) = p `GHC.plusPtr` negate o
+
+offset :: (Pointer p a, Storable b) => Offset p b -> Lens' (p a) (p b)
+offset o = let
+  getter p = p `addOffset` o
+  setter = undefined
+  in lens getter setter
 
 data Memory p m a where
   ReadMem :: Pointer p n => p n -> Memory p m n
