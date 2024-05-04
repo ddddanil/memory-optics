02bbc9e464c1931f0899ffb1cb9fb12597370731
Danil Doroshin <ddddanil5555@gmail.com>
2024-05-04 15:18:11 +0300

    fix: make offset optic a getter

--- src/Data/Memory.hs
+++ src/Data/Memory.hs
@@ -1,33 +1,32 @@
 module Data.Memory
-( Pointer(Offset, addOffset, subOffset)
+( Pointer(Offset, addOffset)
+, offset
 , Memory(ReadMem, WriteMem), readMem, writeMem
 , runMemoryToIO
 )
 where
 
-import           Control.Lens.Lens (Lens', lens)
-import           Data.Kind         (Type)
-import qualified Foreign.Ptr       as GHC
-import           Foreign.Storable  (Storable (peek, poke))
-import           Polysemy          (Embed, InterpreterFor, Member, interpret,
-                                    makeSem)
-import           Polysemy.Embed    (embed)
+import           Control.Lens.Getter (Getter, to)
+import           Data.Kind           (Type)
+import qualified Foreign.Ptr         as GHC
+import           Foreign.Storable    (Storable (peek, poke))
+import           Polysemy            (Embed, InterpreterFor, Member, interpret,
+                                      makeSem)
+import           Polysemy.Embed      (embed)
 
 class Storable a => Pointer p a where
   data Offset p b :: Type
+  -- | Unsafe. Get a pointer to
   addOffset :: Storable b => p a -> Offset p b -> p b
-  subOffset :: Storable b => p b -> Offset p b -> p a
 
 instance Storable a => Pointer GHC.Ptr a where
   data Offset GHC.Ptr b = GHCPtrOffset Int
   addOffset p (GHCPtrOffset o) = p `GHC.plusPtr` o
-  subOffset p (GHCPtrOffset o) = p `GHC.plusPtr` negate o
 
-offset :: (Pointer p a, Storable b) => Offset p b -> Lens' (p a) (p b)
+offset :: (Pointer p a, Storable b) => Offset p b -> Getter (p a) (p b)
 offset o = let
   getter p = p `addOffset` o
-  setter = undefined
-  in lens getter setter
+  in to getter
 
 data Memory p m a where
   ReadMem :: Pointer p n => p n -> Memory p m n
