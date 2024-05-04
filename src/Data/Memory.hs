0371a1b0f65a59d9a3022d2fbdaaac23449d8fcd
Danil Doroshin <ddddanil5555@gmail.com>
2024-05-04 16:06:57 +0300

    feat: monadic read optic

--- src/Data/Memory.hs
+++ src/Data/Memory.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 module Data.Memory
-( Pointer(Offset, addOffset)
+( Pointer(Offset)
 , offset
 , Memory(ReadMem, WriteMem), readMem, writeMem
+, read
 , runMemoryToIO
 )
 where
@@ -14,25 +16,38 @@ import           Polysemy            (Embed, InterpreterFor, Member, interpret,
                                       makeSem)
 import           Polysemy.Embed      (embed)
 
-class Storable a => Pointer p a where
+class Pointer p where
   data Offset p b :: Type
   -- | Unsafe. Get a pointer to
-  addOffset :: Storable b => p a -> Offset p b -> p b
+  addOffset :: (Storable b) => p a -> Offset p b -> p b
+  -- | Unsafe
+  unsafeCastPointer :: Storable b => p a -> p b
 
-instance Storable a => Pointer GHC.Ptr a where
+instance Pointer GHC.Ptr where
   data Offset GHC.Ptr b = GHCPtrOffset Int
   addOffset p (GHCPtrOffset o) = p `GHC.plusPtr` o
+  unsafeCastPointer = GHC.castPtr
 
-offset :: (Pointer p a, Storable b) => Offset p b -> Getter (p a) (p b)
+offset :: (Pointer p, Storable b) => Offset p b -> Getter (p a) (p b)
 offset o = let
   getter p = p `addOffset` o
   in to getter
 
 data Memory p m a where
-  ReadMem :: Pointer p n => p n -> Memory p m n
-  WriteMem :: Pointer p n => n -> p n -> Memory p m ()
+  ReadMem :: (Storable n, Pointer p) => p n -> Memory p m n
+  WriteMem :: (Storable n, Pointer p) => n -> p n -> Memory p m ()
 
 makeSem ''Memory
+
+read :: forall r p a b. (Storable a, Storable b, Pointer p, Member (Memory p) r) => MonadicOptic (Sem r) (p a) (p b) a b
+read = let
+  read_ :: p a -> Sem r (a, p b)
+  read_ ptr = do
+    val <- readMem ptr
+    return (val, unsafeCastPointer ptr)
+  write_ :: p b -> b -> Sem r (p b)
+  write_ ptr d = writeMem d ptr >> return ptr
+  in monadicOptic @(Sem r) (ExistentialMonadicOptic read_ write_)
 
 runMemoryToIO :: Member (Embed IO) r => InterpreterFor (Memory GHC.Ptr) r
 runMemoryToIO = interpret \case
