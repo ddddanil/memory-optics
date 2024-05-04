9e01eacfc3c1221acb360fdd9dd54023040676a4
Danil Doroshin <ddddanil5555@gmail.com>
2024-05-04 17:52:33 +0300

    feat: move nativemem to an effect

--- src/Data/Memory.hs
+++ src/Data/Memory.hs
@@ -1,21 +1,30 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 module Data.Memory
-( Pointer(Offset)
-, offset
-, NativeType(readMem, writeMem)
-, read
-)
+  ( Pointer(Offset)
+  , offset
+  , NativeType
+  , readNative
+  , Memory(ReadMem, WriteMem)
+  , readMem, writeMem
+  , read
+  , runMemoryAsNative
+  , runMemoryMonadIO
+  )
 where
 
 import           Control.Lens.Getter  (Getter, to)
 import           Control.Lens.Monadic (ExistentialMonadicOptic (ExistentialMonadicOptic),
                                        MonadicOptic, monadicOptic)
 import           Control.Monad        (Monad (return, (>>)))
+import           Data.Function        (($))
 import           Data.Kind            (Type)
 import           Foreign              (Int)
 import qualified Foreign.Ptr          as GHC
 import           Foreign.Storable     (Storable (peek, poke))
 import           GHC.IO               (IO)
+import           Polysemy             (Embed, InterpreterFor, Member, Sem,
+                                       embed, interpret, makeSem)
+import           Polysemy.Embed       (runEmbedded)
 
 class Pointer p where
   data Offset p b :: Type
@@ -29,26 +38,51 @@ offset o = let
   getter p = p `addOffset` o
   in to getter
 
-class NativeType p m a where
-  readMem :: p a -> m a
-  writeMem :: p a -> a -> m ()
+class (Pointer p) => NativeType p a where
+  data MemoryMonad p a :: Type
+  readMemM :: p a -> MemoryMonad p a
+  writeMemM :: p a -> a -> MemoryMonad p a
 
-read :: forall m p a b. (Monad m, Pointer p, NativeType p m a, NativeType p m b) => MonadicOptic m (p a) (p b) a b
+readNative :: (NativeType p a, NativeType p b, Monad (MemoryMonad p)) => MonadicOptic (MemoryMonad p) (p a) (p b) a b
+readNative = let
+  read_ :: (NativeType p a, Monad (MemoryMonad p)) => p a -> (MemoryMonad p) (a, p b)
+  read_ ptr = do
+    val <- readMemM ptr
+    return (val, unsafeCastPointer ptr)
+  write_ :: (NativeType p b, Monad (MemoryMonad p)) => p b -> b -> (MemoryMonad p) (p b)
+  write_ ptr d = writeMemM ptr d >> return ptr
+  in monadicOptic (ExistentialMonadicOptic read_ write_)
+
+data Memory p m a where
+  ReadMem :: (NativeType p a) => p a -> Memory p m a
+  WriteMem :: (NativeType p a) => p a -> a -> Memory p m a
+
+makeSem ''Memory
+
+read :: forall r p a b. (NativeType p a, NativeType p b, Member (Memory p) r) => MonadicOptic (Sem r) (p a) (p b) a b
 read = let
-  read_ :: p a -> m (a, p b)
+  read_ :: p a -> Sem r (a, p b)
   read_ ptr = do
     val <- readMem ptr
     return (val, unsafeCastPointer ptr)
-  write_ :: p b -> b -> m (p b)
+  write_ :: p b -> b -> Sem r (p b)
   write_ ptr d = writeMem ptr d >> return ptr
-  in monadicOptic @m (ExistentialMonadicOptic read_ write_)
+  in monadicOptic (ExistentialMonadicOptic @_ @(Sem r) read_ write_)
+
+runMemoryAsNative :: (Member (Embed (MemoryMonad p)) r) => InterpreterFor (Memory p) r
+runMemoryAsNative = interpret $ \case
+  ReadMem ptr -> embed $ readMemM ptr
+  WriteMem ptr d -> embed $ writeMemM ptr d
 
 instance Pointer GHC.Ptr where
   data Offset GHC.Ptr b = GHCPtrOffset Int
   addOffset p (GHCPtrOffset o) = p `GHC.plusPtr` o
   unsafeCastPointer = GHC.castPtr
 
-instance Storable a => NativeType GHC.Ptr IO a where
-  readMem = peek
-  writeMem = poke
+instance Storable a => NativeType GHC.Ptr a where
+  data MemoryMonad GHC.Ptr a = MemoryMonadIO (IO a)
+  readMemM ptr = MemoryMonadIO $ peek ptr
+  writeMemM ptr d = MemoryMonadIO $ poke ptr d >> return d
 
+runMemoryMonadIO :: Member (Embed IO) r => InterpreterFor (Embed (MemoryMonad GHC.Ptr)) r
+runMemoryMonadIO = runEmbedded $ \(MemoryMonadIO m) -> m
