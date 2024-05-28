37b836db356d79a43d52837f8cbf65676041800b
Danil Doroshin <ddddanil5555@gmail.com>
2024-05-28 18:33:57 +0300

    feat: add more C layout tests

--- test/Spec/Abi/C.hs
+++ test/Spec/Abi/C.hs
@@ -22,11 +22,15 @@ import           Foreign.Ptr           (Ptr)
 import           GHC.Err               (undefined)
 import           GHC.Generics          (Generic)
 import           GHC.Show              (Show)
-import           GHC.Word              (Word8)
+import           GHC.Word              (Word16, Word8)
 
 tests = fromGroup $$(discover)
 
 -- | * Layout tests
+
+-- | ** Only native fields
+
+-- | ** Layout 01
 
 data Layout_01 f
   = Layout_01
@@ -53,6 +57,70 @@ prop_Layout_01_expect_layout = let
   expectLayout = Layout_01
     { f1 = OffsetFor . unsafeOffsetFromBytes (Proxy @(Ptr (Layout_01 Identity), Ptr Word8)) $ 0
     , f2 = OffsetFor . unsafeOffsetFromBytes (Proxy @(Ptr (Layout_01 Identity), Ptr Word8)) $ 1
+    }
+  in property . test $ do
+    actualSize === expectSize
+    actualLayout === expectLayout
+
+-- | Layout 02
+
+data Layout_02 f
+  = Layout_02
+  { f1 :: f Word16
+  , f2 :: f Word8
+  }
+  deriving ( Generic, FunctorB, TraversableB, ApplicativeB, ConstraintsB )
+
+deriving instance AllBF Show f Layout_02 => Show (Layout_02 f)
+deriving instance AllBF Eq   f Layout_02 => Eq   (Layout_02 f)
+
+prop_Layout_02_expect_layout :: Property
+prop_Layout_02_expect_layout = let
+  (actualSize, actualLayout)
+    = greedyStructLayout @Layout_02 @Ptr
+      (Layout_02
+        { f1 = Const undefined
+        , f2 = Const undefined
+        })
+  expectSize = SizeOf
+    { sizeOf = 3
+    , alignOf = 2
+    }
+  expectLayout = Layout_02
+    { f1 = OffsetFor . unsafeOffsetFromBytes (Proxy @(Ptr (Layout_02 Identity), Ptr Word8)) $ 0
+    , f2 = OffsetFor . unsafeOffsetFromBytes (Proxy @(Ptr (Layout_02 Identity), Ptr Word8)) $ 2
+    }
+  in property . test $ do
+    actualSize === expectSize
+    actualLayout === expectLayout
+
+-- | Layout 03
+
+data Layout_03 f
+  = Layout_03
+  { f1 :: f Word8
+  , f2 :: f Word16
+  }
+  deriving ( Generic, FunctorB, TraversableB, ApplicativeB, ConstraintsB )
+
+deriving instance AllBF Show f Layout_03 => Show (Layout_03 f)
+deriving instance AllBF Eq   f Layout_03 => Eq   (Layout_03 f)
+
+prop_Layout_03_expect_layout :: Property
+prop_Layout_03_expect_layout = let
+  (actualSize, actualLayout)
+    = greedyStructLayout @Layout_03 @Ptr
+      (Layout_03
+        { f1 = Const undefined
+        , f2 = Const undefined
+        })
+  expectSize = SizeOf
+    { sizeOf = 4
+    , alignOf = 2
+    }
+  expectLayout = Layout_03
+    { f1 = OffsetFor . unsafeOffsetFromBytes (Proxy @(Ptr (Layout_03 Identity), Ptr Word8)) $ 0
+    , f2 = OffsetFor . unsafeOffsetFromBytes (Proxy @(Ptr (Layout_03 Identity), Ptr Word8)) $ 2
     }
   in property . test $ do
     actualSize === expectSize
